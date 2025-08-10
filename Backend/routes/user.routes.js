8b94f4508364b27811f3f1be14c7ada263c90a15
Sagar Paul <sagarpaul9475@gmail.com>
2025-08-10 17:01:39 +0530

    user authentication done

--- Backend/routes/user.routes.js
+++ Backend/routes/user.routes.js
@@ -2,6 +2,8 @@ const express= require('express');
 const router = express.Router();
 const {body} = require('express-validator');
 const userController = require('../controllers/user.controller');
+const authMiddleware = require('../middlewares/auth.middleware');
+
 
 module.exports = router;
 
@@ -17,6 +19,10 @@ router.post('/login',[
 ],
   userController.loginUser
 )
+
+
+router.get('/profile',authMiddleware.authUser,userController.getUserProfile)
+router.get('/logout', authMiddleware.authUser,userController.logoutUser)
 
 
 module.exports = router
